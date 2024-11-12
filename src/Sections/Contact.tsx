a4343e5261090a8f5026a6f9dc50108d9c113d46
ChristophTabTeam <101418576+ChristophTabTeam@users.noreply.github.com>
2024-11-12 18:53:20 +0100

    added firebase hosting

--- src/Sections/Contact.tsx
+++ src/Sections/Contact.tsx
@@ -1,6 +1,35 @@
 import React from "react";
+import { useLanguage } from "../context/useLanguage";
 
 const Contact: React.FC = () => {
+  const { language } = useLanguage();
+
+  if (language === "en") {
+    return (
+      <div className="section">
+        <div className="container">
+          <div className="contact-wrapper">
+            <div className="head-wrapper">
+              <h2 className="display-2 text-dark-grey">Contact</h2>
+            </div>
+            <div className="contact-wrapper">
+              <p className="large">
+                Write me an email to
+                <br />
+                <a
+                  href="mailto:mail@christophlabestin.de"
+                  className="display-5"
+                >
+                  mail@christophlabestin.de
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="section">
       <div className="container">
@@ -10,7 +39,8 @@ const Contact: React.FC = () => {
           </div>
           <div className="contact-wrapper">
             <p className="large">
-              Schreiben Sie mir eine E-Mail an<br />
+              Schreiben Sie mir eine E-Mail an
+              <br />
               <a href="mailto:mail@christophlabestin.de" className="display-5">
                 mail@christophlabestin.de
               </a>
